65d0a8cee2d8f29b453b20b1a334a9f5173a7f28
pzx3 <55480017+pzx3@users.noreply.github.com>
2025-02-28 20:37:29 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "NetfilmPlayer",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v13),
         .macOS(.v10_15)
     ],
     products: [
